65526018fb5f13b2066dd7e0ef5ce557dcfcb0d9
AlessandraSantanna <alesy2007@gmail.com>
2026-04-30 04:03:30 -0300

    adiciona tabela

--- sos-enchentes-backend/src/database/db.js
+++ sos-enchentes-backend/src/database/db.js
@@ -1,5 +1,6 @@
 import pkg from "pg";
 const { Pool } = pkg;
+import { pool } from "./db.js";
 
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
@@ -7,4 +8,10 @@ export const pool = new Pool({
     rejectUnauthorized: false,
   },
 });
+
+pool.connect()
+  .then(() => console.log("✅ Conectado ao banco!"))
+  .catch(err => console.error("❌ Erro ao conectar:", err));
+
+  
 //postgresql://desafiofinal_27bu_user:2hRkhxd9YU4pmeyvT65RZSKPQSPrdfaj@dpg-d7pfhasvikkc73adlgl0-a.oregon-postgres.render.com/desafiofinal_27bu
