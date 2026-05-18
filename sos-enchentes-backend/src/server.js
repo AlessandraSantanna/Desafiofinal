23d7ad06ace241386c1c50838dea049e13762587
AlessandraSantanna <alesy2007@gmail.com>
2026-05-18 02:07:26 -0300

    corrige cors put

--- sos-enchentes-backend/src/server.js
+++ sos-enchentes-backend/src/server.js
@@ -12,7 +12,7 @@ const app = express();
 /* 🔥 Middlewares */
 app.use(cors({
   origin: "*",
-  methods: ["GET", "POST", "PATCH", "DELETE"],
+  methods: ["GET", "POST", "PUT","PATCH", "DELETE"],
   allowedHeaders: ["Content-Type"]
 }));
 
